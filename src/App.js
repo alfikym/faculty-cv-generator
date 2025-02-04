8b318e8ad827653121c86452e82ee4604028437e
alfikym <alfiky@gmail.com>
2025-02-04 21:06:20 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FacultyCVGenerator from "./FacultyCVGenerator";
+
+function App() {
+  return (
+    <div>
+      <h1>Faculty CV Generator</h1>
+      <FacultyCVGenerator />
+    </div>
+  );
+}
+
+export default App;
